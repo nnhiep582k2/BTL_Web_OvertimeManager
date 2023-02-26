66c24c7cf6b5d535fe6876136c67d9619b25abdc
Nguyen Ngoc Hiep <nnhiep582k2@gmail.com>
2023-02-27 06:25:24 +0700

    feat: adding template to project

---
+++ OVERTIME.MANAGER.MAIN/Controllers/BlogController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OVERTIME.MANAGER.MAIN.Controllers
+{
+    public class BlogController : Controller
+    {
+        // Trang chủ - Blog
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Trang chi tiết 1 blog
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
