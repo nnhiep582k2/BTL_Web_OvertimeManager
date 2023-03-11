2b7c36b4f0ebb82ca78bcc03cc0aad2a867fbf66
Nguyen Ngoc Hiep <nnhiep582k2@gmail.com>
2023-03-12 00:05:00 +0700

    feat: fix some bug for npBac

--- OVERTIME.MANAGER.MAIN/wwwroot/assets/components/register/js/regis.js
+++ OVERTIME.MANAGER.MAIN/wwwroot/assets/components/register/js/regis.js
@@ -1,48 +1,39 @@
 function ShowError(input, message) {
     let parent = input.parentElement;
-    let small = parent.querySelector('small')
-
-    small.innerText = message
-
-
+    let small = parent.querySelector('small');
+    small.innerText = message;
 }
+
 function ShowSuccess(input, message) {
     let parent = input.parentElement;
-    let small = parent.querySelector('small')
-
-    small.innerText = ''
-
-
+    let small = parent.querySelector('small');
+    small.innerText = '';
 }
-
-
 
 function checkEmptyError(listInput) {
-    let isEmptyError = false
+    let isEmptyError = false;
     listInput.forEach(input => {
-
-        input.value = input.value.trim()
+        input.value = input.value.trim();
         if (!input.value) {
-            isEmptyError = true
-            ShowError(input, 'not be empty!')
+            isEmptyError = true;
+            ShowError(input, 'not be empty!');
         }
         else {
-            ShowSuccess(input)
+            ShowSuccess(input);
         }
+    });
 
-    })
-
-    return isEmptyError
+    return isEmptyError;
 }
+
 var fullname = document.getElementById("fullname")
 var email = document.getElementById("email")
 var username = document.getElementById("username")
 var pass = document.getElementById("password")
 var repass = document.getElementById("repassword")
-
 var singup_btn = document.querySelector(".submit")
-singup_btn.addEventListener('click', function (e) {
 
-    e.preventDefault()
-    checkEmptyError([fullname, email, username, pass, repass])
+singup_btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    checkEmptyError([fullname, email, username, pass, repass]);
 })
