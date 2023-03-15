f02d947be5082a2ebfe90633cf3dcfeaffaaf9fe
Nguyen Ngoc Hiep <nnhiep582k2@gmail.com>
2023-03-15 23:38:39 +0700

    feat: fix header

--- OVERTIME.MANAGER.MAIN/Controllers/OvertimeManagerController.cs
+++ OVERTIME.MANAGER.MAIN/Controllers/OvertimeManagerController.cs
@@ -8,5 +8,55 @@ namespace OVERTIME.MANAGER.MAIN.Controllers
         {
             return View();
         }
+
+        /// <summary>
+        /// Quản lý ca làm việc
+        /// </summary>
+        /// <returns>View</returns>
+        /// @author nnhiep 15.03.2023
+        public IActionResult WorkingShiftManager()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Quản lý thời điểm làm thêm
+        /// </summary>
+        /// <returns>View</returns>
+        /// @author nnhiep 15.03.2023
+        public IActionResult OvertimeWorkingShiftManager()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Quản lý vị trí công việc
+        /// </summary>
+        /// <returns>View</returns>
+        /// @author nnhiep 15.03.2023
+        public IActionResult JobPositionManager()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Quản lý đơn vị công tác
+        /// </summary>
+        /// <returns>View</returns>
+        /// @author nnhiep 15.03.2023
+        public IActionResult OrganizationManager()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Quản lý nhân viên
+        /// </summary>
+        /// <returns>View</returns>
+        /// @author nnhiep 15.03.2023
+        public IActionResult EmployeeManager()
+        {
+            return View();
+        }
     }
 }
---
+++ OVERTIME.MANAGER.MAIN/ViewModels/NavigatorActive.cs
@@ -0,0 +1,7 @@
+﻿namespace OVERTIME.MANAGER.MAIN.ViewModels
+{
+    public class NavigatorActive
+    {
+        public string navigatorActive { get; set; } = "Index";
+    }
+}
