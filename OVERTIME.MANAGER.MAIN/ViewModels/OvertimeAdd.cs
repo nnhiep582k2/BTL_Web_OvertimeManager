8dd851aea3943a6107404e7d648f388adbd70f9d
Nguyen Ngoc Hiep <nnhiep582k2@gmail.com>
2023-04-04 23:04:46 +0700

    feat: add details

---
+++ OVERTIME.MANAGER.MAIN/ViewModels/OvertimeAdd.cs
@@ -0,0 +1,16 @@
+﻿using OVERTIME.MANAGER.MAIN.Models;
+using X.PagedList;
+
+namespace OVERTIME.MANAGER.MAIN.ViewModels
+{
+    public class OvertimeAdd
+    {
+        #nullable disable
+        public Overtime overtime { get; set; }
+        public PagedList<Employee> list { get; set; }
+        public string searchQuery { get; set; }
+        public int pageIndex { get; set; } = 1;
+        public int pageSize { get; set; } = 15;
+        public int totalRecord { get; set; } = 0;
+    }
+}
